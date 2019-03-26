4b2423692a81678b51facc4dbf1879a059252826
Colton Lacey and Stuart McKay <stuart@>
2019-03-26 11:07:28 -0700

    add sick pics

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#baby-showing").toggle();
     $("#baby-hidden").toggle();
+    $("#img").fadeToggle();
+  });
+  $(".clickableToo").click(function() {
+    $("#more-pics-showing").toggle();
+    $("#more-pics-hidden").toggle();
   });
 });
